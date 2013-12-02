ed704f19fb01769e8e1ee958cba9592ed1163341
Gereon Kaiping <gereon@veredus>
2013-12-02 10:19:57 +0000

    Improved my own inference code

--- inference.py
+++ inference.py
@@ -1,3 +1,5 @@
+# -*- encoding: utf-8
+
 import numpy
 import operator
 from functools import reduce
@@ -85,11 +87,37 @@ class Node:
         
 
 class TreeAugmentedClassifier:
+    """A tree-augmented naive Bayes classifier
+
+    The network uses Pearl's belief propagation for updating
+    beliefs.
+
+    """
     def __init__(self, classes, a_priori_probabilities):
+        """ Initialize a TAN, starting with a Class node.
+
+        classes: A sequence of class titles.
+        a_priori_probabilities: A sequence of the a priori probabilities of these classes in the same order
+        """
+        # Should this be passed a {class:p} dictionary instead?
         self.classes = Node(a_priori_probabilities, [], "class", classes)
-        self.nodes = []
+        self.nodes = {}
         
     def add_node(self, p, name, labels=None, parent=None):
+        """Add a new node to the network.
+
+        The node will have the class as parent, as well as up to one
+        already defined node.
+
+        p: The probability table of the node to be added, of shape (classes, parent_values, own_values)
+        name: The name (a hashable) of the new node.
+        labels (optional): labels for the options of this node
+        parent: A Node instance
+
+        Returns: the node constructed, so that it can be passed to
+        later calls of this method.
+
+        """
         #p: conditional_probabilities_by_class
         p = numpy.asarray(p)
         if parent:
@@ -101,15 +129,20 @@ class TreeAugmentedClassifier:
             cl, pr = p.shape
             assert(cl == len(self.classes.labels))
             new = Node(p, [self.classes], name, labels)
-        self.nodes.append(new)
+        self.nodes[name] = new
         return new
     
     def inference(self):
+        """Calculate the probability of the classification, based on the
+evidence present in the Nodes."""
         self.classes.down()
-        for node in self.nodes:
+        for node in self.nodes.values():
             node.down()
         for node in reversed(self.nodes):
             node.up()
         self.classes.down()
         return self.classes.belief()
+
+# Implement incremental learning according to Alcobé: Incremental Learning of Tree Augmented Naive Bayes Classifiers (http://www.lsi.us.es/iberamia2002/confman/SUBMISSIONS/102-osetesuure.pdf)
+
 
